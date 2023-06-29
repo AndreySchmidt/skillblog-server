4b2a3ce132ccbcaf314b8258beb23349a12fe590
AndreySchmidt <ringnull@gmail.com>
2023-06-29 13:15:15 +0300

    login logout

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,18 @@
 import {
   Body,
   Controller,
+  Get,
   Header,
   HttpCode,
   HttpStatus,
   Post,
+  Request,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { LocalAuthGuard } from 'src/auth/local.auth.guard';
+import { AunthenticatedGuard } from 'src/auth/authenticated.guard';
 
 // http//:localhost:3000/users/
 @Controller('users')
@@ -20,5 +25,27 @@ export class UsersController {
   @Header('Content-type', 'application/json')
   createUser(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
+  }
+
+  // http//:localhost:3000/users/login
+  @Post('/login')
+  @UseGuards(LocalAuthGuard)
+  @HttpCode(HttpStatus.OK)
+  login(@Request() req) {
+    return { user: req.user, msg: 'Logged in' };
+  }
+
+  // http//:localhost:3000/users/login-check
+  @Get('/login-check')
+  @UseGuards(AunthenticatedGuard)
+  loginCheck(@Request() req) {
+    return req.user;
+  }
+
+  // http//:localhost:3000/users/logout
+  @Get('/logout')
+  logout(@Request() req) {
+    req.session.destroy();
+    return { msg: 'Session has ended' };
   }
 }
